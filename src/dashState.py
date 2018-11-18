8a7b09d2fa714e457ed878a657ac2ed0e7bc0ac4
Marvin Taschenberger <marvin.taschenberger@gmail.com>
2018-11-18 15:16:20 +0100

    Finished Part 4 of the Tutorial

--- src/dashState.py
+++ src/dashState.py
@@ -10,13 +10,15 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 app.layout = html.Div([
     dcc.Input(id="input-1", type="text", value="Los Angeles"),
     dcc.Input(id="input-2", type="text", value="Germany"),
+    html.Button(id="submit-button", n_clicks=0, children="Submit"),
     html.Div(id="out-div")
     ])
 
 @app.callback(Output("out-div", "children"),
-             [Input("input-1", "value"),
-              Input("input-2", "value")])
-def update_text(in1, in2):
+              [Input("submit-button", "n_clicks")],
+             [State("input-1", "value"),
+              State("input-2", "value")])
+def update_text(n_clicks, in1, in2):
     return f""" Input 1 is {in1} and Input 2 is {in2}"""
 
 if __name__ == "__main__":
